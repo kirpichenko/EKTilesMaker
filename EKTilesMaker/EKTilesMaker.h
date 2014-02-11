2ac11dc20ec75cb9867b3fc3fb04f05618b9babb
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2014-02-11 16:32:23 +0200

    Add documentation comments

--- EKTilesMaker/EKTilesMaker.h
+++ EKTilesMaker/EKTilesMaker.h
@@ -14,18 +14,38 @@ typedef NS_ENUM(NSInteger, OutputFileType)
     OutputFileTypeJPG
 };
 
+/**
+ Class that create tiles based on the provided source image
+ */
 @interface EKTilesMaker : NSObject
 
 - (void)createTiles;
 
+///Path to the source image
 @property (nonatomic) NSString *sourceImagePath;
+
+///Output folder path for tiles storing
 @property (nonatomic) NSString *outputFolderPath;
+
+/// Name of output tiles
 @property (nonatomic) NSString *outputFileName;
 
+/** 
+ Array of zoom levels for tiles creating. If @[@1, @0.5] scales are provided then tiles will be
+ created for original image with x1 and x0.5 scales
+*/
 @property (nonatomic) NSArray *zoomLevels;
+
+/// The size of output tiles images
 @property (nonatomic) CGSize tileSize;
+
+/**
+ Image type of tiles. Provide OutputfileTypePNG for storing tiles in png format or
+ OutputfileTypePNG for storing tiles as jpg. Default value is OutputfileTypePNG
+ */
 @property (nonatomic) OutputFileType outputFileType;
 
+///Completion block to be called after tiling process did finish
 @property (nonatomic, copy) void(^completionBlock)();
 
 @end
--- EKTilesMaker/UIImage+EKTilesMaker.h
+++ EKTilesMaker/UIImage+EKTilesMaker.h
@@ -10,9 +10,25 @@
 
 @interface UIImage (EKTilesMaker)
 
+/*! Gets image at path and scales it to provided scale
+ \param scale Scaling value for image
+ \param imagePath Path for source image
+ \returns Scaled image at path
+ */
 + (UIImage *)imageWithScale:(CGFloat)scale atPath:(NSString *)imagePath;
 
+
+/*! Scales image to provided scale
+ \param scale Scaling value to be applied to the original image
+ \returns Sclaed original image
+ */
 - (UIImage *)imageWithScale:(CGFloat)scale;
+
+
+/*! Extracts image in rect from original image
+ \param rect Rectangle to be used for image cropping
+ \returns Image cropped from original with provided rectangle
+ */
 - (UIImage *)imageInRect:(CGRect)rect;
 
 @end
